cb610face3818ca3ae484f94dc175c7d47fd126d
Utmetu <maciej.mroczko@o2.pl>
2017-06-12 20:08:24 +0200

    dodalem folder/zadania zwiazane z server

---
+++ server/conf.inc.php
@@ -0,0 +1,5 @@
+<?php
+$host = $_SERVER['HTTP_HOST'];
+$obrazy = $host.'/obrazy/';
+
+?>
---
+++ server/index.php
@@ -0,0 +1,21 @@
+<?php
+/* SCRIPT NAME
+include 'header.inc.php';
+
+if (isset($_POST['submit'])) {
+
+echo 'Jeden.';
+}*/
+
+//HOST NAME
+
+//wszystko w miare ok 
+
+require 'conf.inc.php';
+echo '<img src="/c/users/komp/desktop/obrazy/header.jpg"/>';
+//echo '<img src="'.$obrazy.'header.jpg" />';
+//echo $_SERVER['PHP_SELF']; // pokazuje sciezke
+//echo $_SERVER['SERVER_NAME']; // pokazuje nazwe serwera czyli localhost
+//echo $_SERVER['HTTP_HOST']; - to samo co wyzej
+//echo $_SERVER['HTTP_USER_AGENT']; - wow
+?>
